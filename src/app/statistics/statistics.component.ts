1ac56d04333c5f2a7909977417786212283432dc
NivDolev <dolevniv@gmail.com>
2018-09-22 15:38:50 +0300

    Recieving array of picks + winner

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,3 +1,4 @@
+import { HeroPicks } from './models/heroPicks';
 import { Match } from './models/match';
 import { DataService } from './../services/data.service';
 import { Component, OnInit } from '@angular/core';
@@ -16,7 +17,7 @@ export class StatisticsComponent implements OnInit {
   filteredLeagues: any[] = [];
 
   selectedLeague: any;
-  matches: any[] = [];
+  leagueMatches: any[] = [];
 
   constructor(private dataService: DataService) { }
 
@@ -43,19 +44,36 @@ export class StatisticsComponent implements OnInit {
 
   getLeagueIdByName() {
     this.selectedLeague = this.leagues.find(league => league.name === this.inputLeagueName);
-    console.log(this.selectedLeague);
+    // console.log(this.selectedLeague);
   }
 
   getMatchesForSelectedLeague() {
     const sqlQuery = encodeURI(`explorer?sql=select * from matches where leagueid=${this.selectedLeague.leagueid}`);
     this.dataService.getData(sqlQuery).subscribe(matches => {
-      this.matches = matches;
-      console.log(matches.rows);
+      this.selectedLeague = matches;
+      this.getHeroPickes();
     });
   }
 
-  getLeagueGames() {
-    this.dataService.getData('')
+  getHeroPickes() {
+    // change and creat an array of objects that contains the hero picks per team + the winnin team
+    let ind = 0;
+    const matchPicks: any[] = [];
+    this.selectedLeague.rows.forEach(match => {
+      const matchDeatils = {
+        picks: [],
+        winner: false
+      };
+      matchDeatils.picks = match.picks_bans.filter(pick => pick.is_pick === true).sort(this.dynamicSort('team'));
+      matchDeatils.winner = match.radiant_win;
+      matchPicks.push(matchDeatils);
+      if (ind < 6) {
+      console.log(match);
+    }
+      ind++;
+    });
+    console.log(matchPicks);
+
   }
 
   dynamicSort(property) {
