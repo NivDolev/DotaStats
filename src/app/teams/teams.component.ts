1a07f3a0173c679de254a6c36f9a88b553d986cb
NivDolev <dolevniv@gmail.com>
2018-09-26 12:30:47 +0300

    removed "OnDestroy" and cleared team sorting

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -7,7 +7,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.css']
 })
-export class TeamsComponent implements OnInit, OnDestroy {
+export class TeamsComponent implements OnInit {
   loading = true;
   teams = [];
   filteredTeams = [];
@@ -17,12 +17,14 @@ export class TeamsComponent implements OnInit, OnDestroy {
     this.dataService.getData('teams').subscribe(teams => {
       this.loading = false;
       this.teams = teams;
-      this.filteredTeams = teams.sort((a,b) => { 
-        if (a.rating > b.rating) return -1; else if (b.rating > a.rating) return 1; else return 0;
+      this.filteredTeams = teams.sort((a, b) => {
+        if (a.rating > b.rating) {
+          return -1;
+        } else if (b.rating > a.rating) {
+            return 1;
+          } else { return 0; }
       });
     });
   }
 
-  ngOnDestroy() {
-  }
 }
